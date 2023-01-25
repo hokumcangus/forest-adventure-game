18b970260755a3c01d641f6f717d89f3b5ff4ef3
hoku mcangus <mcangusohana2@gmail.com>
2023-01-25 14:31:41 -0800

    Pulling in changes from Unity Project

--- CatController.cs
+++ CatController.cs
@@ -4,36 +4,60 @@ using UnityEngine;
 
 public class CatController : MonoBehaviour
 {
-    public float speed = 3.0f;
-
-    Rigidbody2D rigidbody2d;
-    float horizontal;
-    float vertical;
-
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    private Animator anim;
+    
+    void Start() 
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
-        currentHealth = maxHealth;
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        horizontal = Input.GetAxis("Horizontal"); //this is a update func float # named horizontal Stores input "Horizontal" provides
-        vertical = Input.GetAxis("Vertical");
-    }
-    void FixedUpdate() 
-    {
-        Vector2 position = GetComponent<Rigidbody2D>().position;
-        position.x = position.x + (speed * horizontal) * Time.deltaTime;
-        position.y = position.y + (speed * vertical) * Time.deltaTime;
+    void Update() {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        transform.position += input.normalized * speed * Time.deltaTime;
 
-        GetComponent<Rigidbody2D>().MovePosition(position);
-    }
-    void ChangeHealth(int amount)
-    {
-        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+        if(input != Vector3.zero){
+            anim.SetBool("isRunning", true);
+        else{
+            anim.SetBool("isRunning", false);
+        }
+        
     }
 }
+}
+    
+    // Rigidbody2D rigidbody2d;
+    // float horizontal;
+    // float vertical;
+
+    // Start is called before the first frame update
+    // void Start()
+    // {
+        // rigidbody2d = GetComponent<Rigidbody2D>();
+        // currentHealth = maxHealth;
+    // }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        // horizontal = Input.GetAxis("Horizontal"); //this is a update func float # named horizontal Stores input "Horizontal" provides
+        // vertical = Input.GetAxis("Vertical");
+    // }
+    // void FixedUpdate()/
+        // Vector2 position = GetComponent<Rigidbody2D>().position;
+        // Vector2 position = transform.position;
+        // position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        // position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        // transform.position = position;
+//    }
+        // position.x = position.x + (speed * horizontal) * Time.deltaTime;
+        // position.y = position.y + (speed * vertical) * Time.deltaTime;
+
+        // GetComponent<Rigidbody2D>().MovePosition(position);
+    // }
+    // void ChangeHealth(int amount)
+    // {
+    //     currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+    //     Debug.Log(currentHealth + "/" + maxHealth);
+    // }
+// }
